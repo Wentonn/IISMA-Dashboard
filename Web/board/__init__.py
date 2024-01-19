3369fd2c20a57fdbea04d7702e410d8e37d352cc
Nicolas Owen Halim <64532578+Wentonn@users.noreply.github.com>
2024-01-19 23:09:21 +0700

    Update __init__.py

--- Web/board/__init__.py
+++ Web/board/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+import secrets
 
 from board import requirement,database,intake,home
 
@@ -9,6 +10,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = secrets.token_hex(16)
     app.config.from_prefixed_env()
 
     database.init_app(app)
